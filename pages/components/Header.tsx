6b5ebde68f1d4c0a4741ce3a8937d940b926dba1
Yuri Jean Fabris <yuri@blocknomad.xyz>
2023-04-02 03:45:47 -0300

    feat: add network switch

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,9 +1,22 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { ConnectWallet } from "@thirdweb-dev/react";
-import { useAddress } from "@thirdweb-dev/react";
+import { ConnectWallet, useSwitchChain } from "@thirdweb-dev/react";
+import { useAddress, useNetworkMismatch, ChainId } from "@thirdweb-dev/react";
+import { useEffect } from 'react';
 
 export default function Header() {
+  const address = useAddress(); // Get connected wallet address
+  const switchChain = useSwitchChain(); // Switch to desired chain
+  const isMismatched = useNetworkMismatch(); // Detect if user is connected to the wrong network
+
+  useEffect(() => {
+    // Check if the user is connected to the wrong network
+    if (isMismatched) {
+      // Prompt their wallet to switch networks
+      switchChain(ChainId.Goerli); // the chain you want here
+    }
+  }, [address]);
+
   return (
     <header className="px-48 py-4 shadow border-gray-200 w-full flex items-center space-x-8">
       <Link href="/">
