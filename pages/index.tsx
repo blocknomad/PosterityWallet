39e55688d9e1dbc8eea234e9c536b626e284058e
Yuri Jean Fabris <yuri@blocknomad.xyz>
2023-04-02 02:50:57 -0300

    feat: add banners to landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -20,6 +21,12 @@ export default function Home() {
             <p className=''>Currently, we can transfer value via tokens to anyone, anywhere.</p>
             <p className=''>If you lose your password and even the seed phrase, there are still chances to regain access to your funds.</p>
             <p className=''>Imagine you pass away, will your funds be available to those who are important to you? Posterity is here to help you deal with these problems.</p>
+          </div>
+        </div>
+        <div className='px-48 py-20 my-20 flex items-center flex-col'>
+          <div className='space-y-12 '>
+            <Image src="/how-it-works-successor.png" width="890" height="143" alt="Successor" />
+            <Image src="/how-it-works-heir.png" className='ml-6' width="734" height="149" alt="Heir" />
           </div>
         </div>
       </main>
